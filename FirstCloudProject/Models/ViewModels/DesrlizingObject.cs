9aad17d54adef609b47dabced5284dd72fb02bec
NadaSaleh20 <nadasaleh987@gmail.com>
2022-10-23 02:19:49 +0300

    tyy

---
+++ FirstCloudProject/Models/ViewModels/DesrlizingObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstCloudProject.Models.ViewModels
+{
+    public class DesrlizingObject
+    {
+
+        public string Path { get; set; }
+
+        public string Action { get; set; }
+    }
+}
